265d03f4b4e94977fdbe8e0123551b6fc27bf55a
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2024-03-07 15:38:31 +0100

    Small fix

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,14 @@ import entities.*;
 
 public class Main {
     public static void main(String[] args) {
-        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(123456, 100000, Department.SALES);
-        PartTimeEmployee partTimeEmployee = new PartTimeEmployee(345678, 20, Department.PRODUCTION, 50);
+        FullTimeEmployee fullTimeEmployee = new FullTimeEmployee(123456, 5000, Department.SALES);
+        PartTimeEmployee partTimeEmployee = new PartTimeEmployee(345678, 25, Department.PRODUCTION, 50);
         Manager manager = new Manager(901234, 100000);
         Volunteer volunteer = new Volunteer("John", 25, "CV");
         Employee[] employee = {fullTimeEmployee, partTimeEmployee, manager};
         for (Employee e : employee) {
+            System.out.println("Employee registration number: " + e.getRegistrationNumber());
+
             e.calculateSalary();
         }
         System.out.println("----------------------------------------");
